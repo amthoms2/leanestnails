e5f678dfec727a887010a76d2522642f752dc518
Asia Thomas <amthoms2@gmail.com>
2021-08-31 17:16:36 -0500

    added create react app, navbar created with styling

---
+++ client/nails/src/components/Navbar/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Nav, NavContainer, NavbarLogo } from './NavbarElements'
+
+const Navbar = () => {
+  return (
+   <>
+     <Nav>
+       <NavContainer>
+         <NavbarLogo to='/'>
+           Leanest Nails
+         </NavbarLogo>
+         <h1>hola</h1>
+       </NavContainer>
+     </Nav>
+   </>
+  )
+}
+
+export default Navbar
+
+// 'rafce' creates a React Arrow Function Component with ES7 module system (ES7 React/Redux/GraphQL/React-Native snippets)
+
+// import React from 'react'
+
+// const  = () => {
+//     return (s
+//         <div>
+
+//         </div>
+//     )
