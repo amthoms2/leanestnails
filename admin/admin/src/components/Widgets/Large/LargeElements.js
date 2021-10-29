d49096b6c3a4761805d472ac759347424be3ba6e
Asia Thomas <amthoms2@gmail.com>
2021-10-29 15:28:38 -0500

    functionality for different colored buttons depending on if approved or declined

--- admin/admin/src/components/Widgets/Large/LargeElements.js
+++ admin/admin/src/components/Widgets/Large/LargeElements.js
@@ -5,3 +5,38 @@ export const LargeContainer = styled.div`
   -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
   box-shadow: 0px 5px 12px 7px rgba(0, 0, 0, 0.35);
 `
+export const Title = styled.h3`
+  font-size: 22px;
+  font-weight: 600;
+`
+export const Image = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+export const Table = styled.table`
+
+`
+
+export const TableRow = styled.tr``
+
+export const TableHead = styled.td``
+
+export const User = styled.div``
+
+export const Username = styled.span``
+
+export const Date = styled.td``
+
+export const Amount = styled.td``
+
+export const Status = styled.td``
+
+export const StyledButton = styled.button`
+  padding: 5px 7px;
+  border: none;
+  border-radius: 10px;
+  background-color: ${({type}) => type==="Approved" ? '#01bf71' : '#d80000'}
+`
--- admin/admin/src/components/Widgets/Large/index.js
+++ admin/admin/src/components/Widgets/Large/index.js
@@ -1,11 +1,52 @@
 import React from 'react'
-import {LargeContainer} from "./LargeElements"
+import img from "../../../nailflower.jpg"
+import {LargeContainer, Title, Table, TableHead, TableRow, User, Image, Username, Date, Amount, Status, StyledButton} from "./LargeElements"
 
 const LargeWidget = () => {
+
+  const Button = ({type}) => {
+    console.log(type)
+  return <StyledButton type={type}>{type}</StyledButton>
+  }
   return (
     <>
       <LargeContainer>
-        large
+        <Title>Latest Transactions</Title>
+        <Table>
+          <TableRow>
+            <TableHead>Customer</TableHead>
+            <TableHead>Date</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>Status</TableHead>
+          </TableRow>
+        </Table>
+
+        <Table>
+          <TableRow>
+            <User>
+            <Image src={img} />
+              <Username>Cody Bork</Username>
+            </User>
+            <Date>2 Dec 2021</Date>
+            <Amount>$159.99</Amount>
+            <Status>
+              <Button type="Approved"/>
+            </Status>
+          </TableRow>
+
+          <TableRow>
+            <User>
+            <Image src={img} />
+              <Username>Cody Bork</Username>
+            </User>
+            <Date>2 Nov 2021</Date>
+            <Amount>$179.99</Amount>
+            <Status>
+              <Button type="Declined" />
+            </Status>
+          </TableRow>
+
+        </Table>
       </LargeContainer>
     </>
   )
