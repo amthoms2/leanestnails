85914f4b529507d651af30bee954b2d9895ea1ba
Asia Thomas <amthoms2@gmail.com>
2021-11-15 18:04:57 -0600

    completed functionality for admin to create new product including storing file upload to google firebase and storing in db

---
+++ admin/admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABzxkjhf-WPiCcVPgs3qNKmnF09ZePu0s",
+  authDomain: "leanest-nails.firebaseapp.com",
+  projectId: "leanest-nails",
+  storageBucket: "leanest-nails.appspot.com",
+  messagingSenderId: "924264899754",
+  appId: "1:924264899754:web:24d30ac8f3989472a31cd3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
--- admin/admin/src/pages/newProduct/index.js
+++ admin/admin/src/pages/newProduct/index.js
@@ -1,84 +1,178 @@
-import React from 'react'
-import {NewProductContainer, Title, Form, Item, NewProductButton, Input} from "./NewProductElements.js"
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import {
+  NewProductContainer,
+  Title,
+  Form,
+  Item,
+  NewProductButton,
+  Input,
+} from "./NewProductElements.js";
+import app from "../../firebase";
+import { addProducts } from "../../redux/api.js";
 
 const NewProduct = () => {
+  const [inputs, setInputs] = useState({});
+  const [imgFile, setImgFile] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleFile = (evt) => {
+    setImgFile(evt.target.files[0]);
+  };
+
+  const handleChange = (evt) => {
+    setInputs((prev) => {
+      return { ...prev, [evt.target.name]: evt.target.value };
+    });
+  };
+
+  const handleClick = (evt) => {
+    evt.preventDefault();
+    //this will prevent files w/ same name to override each other
+    const fileName = new Date().getTime() + imgFile.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+    //upload process begins here
+    const uploadTask = uploadBytesResumable(storageRef, imgFile);
+
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const product = {...inputs, img:downloadURL };
+          addProducts(product, dispatch)
+        });
+      }
+    );
+  };
+  console.log(inputs);
+  console.log(imgFile);
   return (
     <>
-    <NewProductContainer>
-      <Title>New Product</Title>
-      <Form>
-        <Item>
-          <label><b>Image</b></label>
-          <Input type="file"
-            id="file"></Input>
-        </Item>
+      <NewProductContainer>
+        <Title>New Product</Title>
+        <Form>
+          <Item>
+            <label>
+              <b>Image</b>
+            </label>
+            <Input
+              type="file"
+              name="img"
+              id="file"
+              onChange={handleFile}
+            ></Input>
+          </Item>
 
-        <Item>
-          <label>Title</label>
-          <Input name="title" type="text" placeholder="Product Name"></Input>
-        </Item>
+          <Item>
+            <label>Title</label>
+            <Input
+              name="title"
+              type="text"
+              placeholder="Product Name"
+              onChange={handleChange}
+            ></Input>
+          </Item>
 
-        <Item>
-          <label>Description</label>
-          <Input name="desc" type="text" placeholder="Description"></Input>
-        </Item>
+          <Item>
+            <label>Description</label>
+            <Input
+              name="desc"
+              type="text"
+              placeholder="Description"
+              onChange={handleChange}
+            ></Input>
+          </Item>
 
-        <Item>
-          <label>Price</label>
-          <Input name="price" type="number" placeholder="Amount"></Input>
-        </Item>
+          <Item>
+            <label>Price</label>
+            <Input
+              name="price"
+              type="number"
+              placeholder="Amount"
+              onChange={handleChange}
+            ></Input>
+          </Item>
 
-        <Item>
-          <label>Stock</label>
-          <select>
-            <option disabled selected></option>
-            <option value="true">Yes</option>
-            <option value="false">No</option>
-          </select>
-        </Item>
+          <Item>
+            <label>Stock</label>
+            <select name="inStock" onChange={handleChange}>
+              <option disabled selected></option>
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </Item>
 
-        <Item>
-          <label>Category</label>
-          <select>
-                <option disabled selected></option>
-                <option value="press-ons">Press Ons</option>
-                <option value="custom">Custom Press ons</option>
-          </select>
-        </Item>
+          <Item>
+            <label>Category</label>
+            <select name="categories" onChange={handleChange}>
+              <option disabled selected></option>
+              <option value="press-ons">Press Ons</option>
+              <option value="custom">Custom Press ons</option>
+            </select>
+          </Item>
 
-        <Item>
-          <label>Nail Shape</label>
-          <select>
-            <option disabled selected></option>
-            <option value="Short Oval">Short Oval</option>
-            <option value="Long Stiletto">Long Stiletto</option>
-            <option value="Short Square">Short Square</option>
-            <option value="other">Other</option>
-          </select>
-        </Item>
+          <Item>
+            <label>Nail Shape</label>
+            <select name="shape" onChange={handleChange}>
+              <option disabled selected></option>
+              <option value="Short Oval">Short Oval</option>
+              <option value="Long Stiletto">Long Stiletto</option>
+              <option value="Short Square">Short Square</option>
+              <option value="other">Other</option>
+            </select>
+          </Item>
 
-        <Item>
-          <label>Color</label>
-          <select>
-          <option disabled selected></option>
-            <option value="black">Black</option>
-            <option value="white">White</option>
-            <option value="red">Red</option>
-            <option value="blue">Blue</option>
-            <option value="yellow">Yellow</option>
-            <option value="green">Green</option>
-            <option value="multicolor">Multicolor</option>
+          <Item>
+            <label>Color</label>
+            <select name="color" onChange={handleChange}>
+              <option disabled selected></option>
+              <option value="black">Black</option>
+              <option value="white">White</option>
+              <option value="red">Red</option>
+              <option value="blue">Blue</option>
+              <option value="yellow">Yellow</option>
+              <option value="green">Green</option>
+              <option value="multicolor">Multicolor</option>
+            </select>
+          </Item>
 
-          </select>
-        </Item>
-
-        <NewProductButton>Create</NewProductButton>
-      </Form>
-   </NewProductContainer>
-
+          <NewProductButton onClick={handleClick}>Create</NewProductButton>
+        </Form>
+      </NewProductContainer>
     </>
-  )
-}
+  );
+};
 
-export default NewProduct
+export default NewProduct;
